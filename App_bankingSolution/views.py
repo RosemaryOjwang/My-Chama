3c2d0f8b3c00844cf8c3c7ed8beb4030fbe5815b
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-03-30 13:13:06 +0300

    add logo

--- App_bankingSolution/views.py
+++ App_bankingSolution/views.py
@@ -3,10 +3,7 @@
 #from django.urls import reverse_lazy
 #from django.views import generic
 from django.views.generic import View
-from django.http import HttpResponse
+
 
 
 # Create your views here.
-class OpenAccount(View):
-    def get(self, request, username):
-        return HttpResponse('Hello' + username + '!')
--- bankingSolution/settings.py
+++ bankingSolution/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
+#from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-t@(ip%pakkt5cm62d0vb*2a$rgal)_k4(1a9_@o9n4djc%sfed'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -85,11 +85,12 @@ LOGOUT_REDIRECT_URL='home'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'NAME': 'banking_solution_db',
+        'USER': 'RA06607',
+        'PASSWORD': 'Kimatui@2020',
         'PORT': '3306',
-        'HOST': config('DB_HOST'),
+        'HOST': '127.0.0.1',
+        
     }
 }
 
